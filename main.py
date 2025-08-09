e7ff0f12b692386c9822d2665ce49d8c1ca4352f
XavierMurillo <xaviermurillosanchez@gmail.com>
2025-08-09 00:54:12 -0400

    Added post process example

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from wifi_con import connect
 from machine import Pin
 import urequests
+import ujson
 
 #Se usa el nombre y password de la red
 SSID = ""
@@ -30,6 +31,18 @@ def get_data():
     status = res.get("status")
     return status
 
+def pulsaciones(press):
+    headers = {"Content-Type": "application/json"}
+    local_url = 'http://192.168.100.87:5000/pressing'
+    data = {
+        "pulsaciones":press
+    }
+    post_data = urequests.post(local_url, data = ujson.dumps(data), headers = headers)
+    print(post_data.text)
+    post_data.close()
+
+
+
 while True:
     #Se lee datos JSON desde pastebin
     try:
@@ -46,5 +59,7 @@ while True:
             else:
                 l1.value(0)
                 l2.value(0)
+        datos_pulsacion = int(input("Ingrese la cantidad de pulsaciones: "))
+        pulsaciones(datos_pulsacion)
     except Exception as e:
         print(e) 
