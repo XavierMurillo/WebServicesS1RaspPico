905336fbb7c086707bd5f9bea98b768bbebe197a
XavierMurillo <xaviermurillosanchez@gmail.com>
2025-08-08 22:50:06 -0400

    Added connection

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from machine import Pin
 import urequests
 
 #Se usa el nombre y password de la red
-SSID = "Entel_Murillo"
-PASS = "M21081966XMS"
+SSID = ""
+PASS = ""
 led = Pin("LED",Pin.OUT)
 l1 = Pin(0, Pin.OUT)
 l2 = Pin(1, Pin.OUT)
@@ -21,7 +21,7 @@ def get_data():
     "X-Master-Key": API_KEY
     }
     url = 'https://api.jsonbin.io/v3/b/68827c1c7b4b8670d8a6cbca'
-    local_url = 'http://192.168.100.12:5000/status' #Se debe usar la ip de la pc, no localhost o 127.0.0.1
+    local_url = 'http://192.168.100.87:5000/status' #Se debe usar la ip de la pc, no localhost o 127.0.0.1
     # data = urequests.get(url, headers = headers)
     data = urequests.get(local_url)
     res = data.json()
@@ -32,16 +32,19 @@ def get_data():
 
 while True:
     #Se lee datos JSON desde pastebin
-    user_text = input("Ingrese la palabra datos para obtener la informacion mas reciente: ")
-    if user_text == "datos":
-        result = get_data()
-        print(f"El status del sistema es \"{result}\"")
-        if "bien" in result:
-            l1.value(1)
-            l2.value(0)
-        elif "mal" in result:
-            l1.value(0)
-            l2.value(1)
-        else:
-            l1.value(0)
-            l2.value(0) 
+    try:
+        user_text = input("Ingrese la palabra datos para obtener la informacion mas reciente: ")
+        if user_text == "datos":
+            result = get_data()
+            print(f"El status del sistema es \"{result}\"")
+            if "bien" in result:
+                l1.value(1)
+                l2.value(0)
+            elif "mal" in result:
+                l1.value(0)
+                l2.value(1)
+            else:
+                l1.value(0)
+                l2.value(0)
+    except Exception as e:
+        print(e) 
